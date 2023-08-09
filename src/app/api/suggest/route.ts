e8ba1eb7dd94b438fae78638de9d9ad9cf80aac1
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-09 17:41:53 +0900

    feat: `/api/suggest` route 정의

---
+++ src/app/api/suggest/route.ts
@@ -0,0 +1,48 @@
+import { NextResponse } from 'next/server';
+
+const OPENAI_URL = 'https://api.openai.com/v1/chat/completions';
+
+const options = {
+  headers: {
+    'Content-Type': 'application/json',
+    Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+  },
+};
+
+const getPayload = (content: string) => {
+  return {
+    model: 'gpt-3.5-turbo',
+    messages: [
+      {
+        role: 'user',
+        content: `The project description and the experiences gained from the project are as follows. 
+        ${content}
+        ${process.env.OPENAI_MESSAGE_CONTENT}`,
+      },
+    ],
+  };
+};
+
+export async function POST(req: Request) {
+  const { content } = await req.json();
+
+  try {
+    const response = await fetch(OPENAI_URL, {
+      method: 'POST',
+      ...options,
+      body: JSON.stringify(getPayload(content)),
+    });
+
+    if (!response.ok) {
+      throw new Error('오류 발생');
+    }
+
+    const data = await response.json();
+
+    const suggestion = data.choices[0].message.content;
+
+    return NextResponse.json({ suggestion });
+  } catch (error) {
+    return NextResponse.json({ error, message: '잠시 후 다시 시도해주세요!' });
+  }
+}
