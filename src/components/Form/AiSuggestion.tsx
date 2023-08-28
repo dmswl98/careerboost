710995081bdc90b48c411bc6b08375c4f7e8fc46
cje <76807107+dmswl98@users.noreply.github.com>
2023-08-29 00:04:00 +0900

    chatGPT 첨삭 내용을 stream으로 응답받기 (#13)
    * feat: 첨삭 내용을 stream으로 응답받기
    
    * chore: `@tanstack/react-query`, `openai` 제거
    
    * remove: fallback 컴포넌트 제거

---
+++ src/components/Form/AiSuggestion.tsx
@@ -0,0 +1,27 @@
+import IconChatGpt from '../Icon/IconChatGpt';
+
+interface AiSuggestionProps {
+  aiSuggestion: string;
+}
+
+const AiSuggestion = ({ aiSuggestion }: AiSuggestionProps) => {
+  return (
+    <div className="px-2 py-2.5 text-slate-700">
+      <div className="mb-3 flex justify-between font-bold">
+        <div>🤖 AI가 첨삭한 내용은 다음과 같아요</div>
+        <p className="text-xs text-slate-500">
+          첨삭 내용을 바탕으로 프로젝트 내용을 수정해보세요
+        </p>
+      </div>
+      {aiSuggestion ? (
+        <p className="py-0.5 text-sm">{aiSuggestion}</p>
+      ) : (
+        <div className="flex justify-center py-4">
+          <IconChatGpt className="h-5 w-10 animate-[rotate_3s_ease-in-out_infinite]" />
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default AiSuggestion;
