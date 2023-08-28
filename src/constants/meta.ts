3fcb5001931bd0fd4309f7eda0af9eba7fa5b1e0
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-08-29 01:45:07 +0900

    chore: meatadata 추가

---
+++ src/constants/meta.ts
@@ -0,0 +1,17 @@
+const META = {
+  TITLE: 'CareerBoost | AI 기반 이력서 첨삭 및 이력서 작성 서비스',
+  SITE_NAME: 'CareerBoost',
+  DESCRIPTION: '이력서 작성과 동시에 AI 첨삭도 받아보세요.',
+  KEYWORD: ['이력서', '첨삭', 'chatgpt', 'gpt이력서', 'ai이력서'],
+  URL: 'https://careerboost.vercel.app',
+  LOCALE: 'ko_KR',
+  TYPE: 'website',
+  CREATOR: '@dmswl',
+  GENERATOR: 'Next.js 13',
+  VIEWPORT: {
+    width: 'device-width',
+    initialScale: 1,
+  },
+} as const;
+
+export default META;
