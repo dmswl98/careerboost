a04289fbd60e18c245fbad80ad14b6f0b5116a6e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-13 20:43:17 +0900

    refactor(Label): `isRequired`에 따른 필수 표시 추가

--- src/components/common/Label.tsx
+++ src/components/common/Label.tsx
@@ -1,18 +1,25 @@
 import { clsx } from 'clsx';
 import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-interface LabelProps extends ComponentPropsWithoutRef<'label'> {}
+import RequireMark from './RequireMark';
+
+interface LabelProps extends ComponentPropsWithoutRef<'label'> {
+  isRequired?: boolean;
+}
 
 const Label = forwardRef<HTMLLabelElement, LabelProps>(
-  ({ className, ...props }, ref) => (
-    <label
-      ref={ref}
-      className={clsx(
-        `text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70`,
-        className
-      )}
-      {...props}
-    />
+  ({ isRequired = false, className, ...props }, ref) => (
+    <>
+      <label
+        ref={ref}
+        className={clsx(
+          `text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70`,
+          className
+        )}
+        {...props}
+      />
+      {isRequired && <RequireMark />}
+    </>
   )
 );
 
---
+++ src/components/common/RequireMark.tsx
@@ -0,0 +1,5 @@
+const RequireMark = () => {
+  return <span className="mb-1 ml-1 inline-block text-gray-400">✱</span>;
+};
+
+export default RequireMark;
