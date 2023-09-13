765f5f279367f819221a89d8925f8ba2d3d7b060
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-13 20:17:04 +0900

    feat: Label 공통 컴포넌트 구현

--- src/components/ui/label.tsx
+++ src/components/ui/label.tsx
@@ -1,26 +1,21 @@
-'use client';
+import { clsx } from 'clsx';
+import { ComponentPropsWithoutRef, forwardRef } from 'react';
 
-import * as LabelPrimitive from '@radix-ui/react-label';
-import { cva, type VariantProps } from 'class-variance-authority';
-import * as React from 'react';
+interface LabelProps extends ComponentPropsWithoutRef<'label'> {}
 
-import { cn } from '@/utils/tailwindMerge';
-
-const labelVariants = cva(
-  'text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70'
+const Label = forwardRef<HTMLLabelElement, LabelProps>(
+  ({ className, ...props }, ref) => (
+    <label
+      ref={ref}
+      className={clsx(
+        `text-sm font-medium peer-disabled:cursor-not-allowed peer-disabled:opacity-70`,
+        className
+      )}
+      {...props}
+    />
+  )
 );
 
-const Label = React.forwardRef<
-  React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root> &
-    VariantProps<typeof labelVariants>
->(({ className, ...props }, ref) => (
-  <LabelPrimitive.Root
-    ref={ref}
-    className={cn(labelVariants(), className)}
-    {...props}
-  />
-));
-Label.displayName = LabelPrimitive.Root.displayName;
+Label.displayName = 'Label';
 
-export { Label };
+export default Label;
