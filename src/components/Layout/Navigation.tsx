1f6f7496a81533d1017933602738e9eec957300c
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-14 23:27:36 +0900

    fix: 로고 경로 수정

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -1,6 +1,5 @@
 import Image from 'next/image';
 import Link from 'next/link';
-import Logo from 'public/logo.png';
 
 import { ROUTES } from '@/constants/routes';
 
@@ -8,7 +7,12 @@ const Navigation = () => {
   return (
     <header className="flex shrink-0 border border-gray-200/70 bg-white px-6 py-3">
       <Link href={ROUTES.HOME} className="w-fit">
-        <Image width={36} height={36} src={Logo} alt="careerboost logo" />
+        <Image
+          width={36}
+          height={36}
+          src={'/logo.png'}
+          alt="careerboost logo"
+        />
       </Link>
     </header>
   );
