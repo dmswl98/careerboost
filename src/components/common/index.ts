1ae136e9b83c9f47e3a0954d17ab43c86f5f15be
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-14 21:13:18 +0900

    feat: Progress 공통 컴포넌트 구현

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,5 +1,6 @@
 export { default as FormErrorMessage } from '@/components/common/FormErrorMessage';
 export { default as Input } from '@/components/common/Input';
 export { default as Label } from '@/components/common/Label';
+export { default as Progress } from '@/components/common/Progress';
 export { default as ServiceErrorMessage } from '@/components/common/ServiceErrorMessage';
 export { default as Textarea } from '@/components/common/Textarea';
