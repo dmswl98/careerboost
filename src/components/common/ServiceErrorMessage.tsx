c39ca38df76ad04c767a35c6bbca425171993c77
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-18 16:32:09 +0900

    refactor(ServiceErrorMessage): route 핸들러가 아닌 route 주소를 prop으로 넘기도록 수정

--- src/components/common/ServiceErrorMessage.tsx
+++ src/components/common/ServiceErrorMessage.tsx
@@ -3,39 +3,40 @@
 import { useRouter } from 'next/navigation';
 
 import { Button } from '@/components/common';
+import { ROUTES } from '@/constants/routes';
 
 const ERROR_MESSAGE = {
   TITLE: '일시적인 오류가 발생했어요',
   DESCRIPTION: '잠시 후 다시 시도해주세요',
-  RESET: '재시도',
+  BUTTON_CONTENT: '재시도',
 };
 
-interface ErrorMessageProps {
+interface ServiceErrorMessageProps {
   title?: string;
   description?: string;
-  reset?: string;
-  onReset?: () => void;
+  buttonContent?: string;
+  route?: (typeof ROUTES)[keyof typeof ROUTES];
 }
 
-const ErrorMessage = ({
+const ServiceErrorMessage = ({
   title = ERROR_MESSAGE.TITLE,
   description = ERROR_MESSAGE.DESCRIPTION,
-  reset = ERROR_MESSAGE.RESET,
-  onReset,
-}: ErrorMessageProps) => {
+  buttonContent = ERROR_MESSAGE.BUTTON_CONTENT,
+  route = ROUTES.HOME,
+}: ServiceErrorMessageProps) => {
   const router = useRouter();
 
-  const handleHomeNavigate = () => {
-    router.push('/');
+  const handleNavigate = () => {
+    router.push(route);
   };
 
   return (
     <div className="my-10 flex flex-col items-center justify-center">
       <p className="mb-1 text-xl font-bold">{title}</p>
       <p className="mb-4">{description}</p>
-      <Button onClick={onReset || handleHomeNavigate}>{reset}</Button>
+      <Button onClick={handleNavigate}>{buttonContent}</Button>
     </div>
   );
 };
 
-export default ErrorMessage;
+export default ServiceErrorMessage;
