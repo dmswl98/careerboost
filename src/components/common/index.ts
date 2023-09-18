381be54bbc4de6cdcb77c9dd096c915be8cbb364
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-19 03:33:08 +0900

    rename(FormErrorMessage): `component/Form` 폴더로 이동

--- src/components/common/index.ts
+++ src/components/common/index.ts
@@ -1,6 +1,5 @@
 export { default as Button } from './Button';
 export { default as Checkbox } from './Checkbox';
-export { default as FormErrorMessage } from './FormErrorMessage';
 export { default as Input } from './Input';
 export { default as Label } from './Label';
 export { default as Progress } from './Progress';
