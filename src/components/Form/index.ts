92c25508ebdab4bea740446ebe869bc20de35ee6
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-20 01:32:45 +0900

    feat: 업무 경력 폼 페이지 구현

--- src/components/Form/index.ts
+++ src/components/Form/index.ts
@@ -1,5 +1,6 @@
 export { default as AiSuggestion } from './AiSuggestion';
 export { default as FormCard } from './FormCard';
 export { default as FormErrorMessage } from './FormErrorMessage';
+export { default as FormRemoveButton } from './FormRemoveButton';
 export { default as MarkdownInput } from './MarkdownInput';
 export { default as PeriodInput } from './PeriodInput';
