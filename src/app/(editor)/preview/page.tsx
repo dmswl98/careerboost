23f4bfa095450e8ffa6d935b83540eb035704e2e
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-21 02:18:56 +0900

    feat: `next/dynamic` 커스텀 loading 컴포넌트 설정

--- src/app/(editor)/preview/page.tsx
+++ src/app/(editor)/preview/page.tsx
@@ -2,8 +2,11 @@
 
 import dynamic from 'next/dynamic';
 
+import { Skeleton } from '@/components/common';
+
 const PdfViewer = dynamic(() => import('../../../components/Pdf/PdfViewer'), {
   ssr: false,
+  loading: () => <Skeleton className="h-[800px] w-[780px]" />,
 });
 
 const Page = () => {
