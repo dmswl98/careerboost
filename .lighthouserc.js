60aed7bfda4b628ef89af5c10313d4865dd9b809
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-09-25 02:32:54 +0900

    chore: lighthouse-ci config

---
+++ .lighthouserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  ci: {
+    collect: {
+      startServerCommand: 'pnpm start',
+      numberOfRuns: 1,
+      url: [
+        'http://localhost:3000',
+        'http://localhost:3000/basic',
+        'http://localhost:3000/experience',
+        'http://localhost:3000/project',
+        'http://localhost:3000/activity',
+        'http://localhost:3000/preview',
+      ],
+      settings: {
+        preset: 'desktop',
+      },
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
