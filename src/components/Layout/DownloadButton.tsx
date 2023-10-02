83664a8e446b9b457ef423aacf11f8abb520fd8a
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-10-03 00:34:25 +0900

    feat(DownloadButton): 이력서 생성 완료 버튼 색상 변경

--- src/components/Layout/DownloadButton.tsx
+++ src/components/Layout/DownloadButton.tsx
@@ -22,7 +22,9 @@ const DownloadButton = () => {
           {({ loading, error }) => (
             <Button
               type="button"
-              className="w-full"
+              className={`w-full ${
+                !loading && !error && 'bg-blue-500 hover:bg-blue-500/90'
+              }`}
               disabled={!!error || loading}
             >
               {loading ? '이력서 생성 중' : '이력서 다운로드'}
