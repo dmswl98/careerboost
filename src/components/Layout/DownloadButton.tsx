8baeea8ec669de92971aea0c6e63ce9b0689fddc
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-10-03 02:06:36 +0900

    fix(DownloadButton): `이력서 생성 완료 버튼` 색상이 변경되지 않는 문제 해결

--- src/components/Layout/DownloadButton.tsx
+++ src/components/Layout/DownloadButton.tsx
@@ -22,10 +22,9 @@ const DownloadButton = () => {
           {({ loading, error }) => (
             <Button
               type="button"
-              className={`w-full ${
-                !loading && !error && 'bg-blue-500 hover:bg-blue-500/90'
-              }`}
-              disabled={!!error || loading}
+              variant={!loading && !error ? 'success' : 'default'}
+              className="w-full"
+              disabled={loading || !!error}
             >
               {loading ? '이력서 생성 중' : '이력서 다운로드'}
             </Button>
