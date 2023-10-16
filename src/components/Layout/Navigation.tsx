958ccb09dbc740334f1f18b593f9ec27e01952ec
weyrm <76807107+dmswl98@users.noreply.github.com>
2023-10-16 18:06:39 +0900

    feat: 언어 변경 기능 구현

--- src/components/Layout/Navigation.tsx
+++ src/components/Layout/Navigation.tsx
@@ -1,12 +1,21 @@
 import Image from 'next/image';
 import Link from 'next/link';
 
-const Navigation = ({ lang }: { lang: string }) => {
+import { type Locale } from '@/i18n/types';
+
+import LanguageSwitcher from './LanguageSwitcher';
+
+interface NavigationProps {
+  lang: Locale;
+}
+
+const Navigation = ({ lang }: NavigationProps) => {
   return (
-    <header className="flex shrink-0 border border-gray-200/70 bg-white px-6 py-3">
+    <header className="flex w-full shrink-0 items-center justify-between border border-gray-200/70 bg-white px-6 py-3">
       <Link href={`/${lang}`} className="w-fit">
         <Image src="/logo.png" width={36} height={36} alt="careerboost logo" />
       </Link>
+      <LanguageSwitcher lang={lang} />
     </header>
   );
 };
